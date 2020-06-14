982cfa1c9abcbc790dfa87c50a82dc3446ef22f8
glaucius <glaucius@gmail.com>
2020-06-14 19:55:47 -0300

    fix

--- k6/script.js
+++
@@ -1,9 +0,0 @@
-import { check } from "k6";
-import http from "k6/http";
-
-export default function() {
-    let res = http.get("https://www.tivit.com/");
-    check(res, {
-        "is status 200": (r) => r.status === 200
-    });
-};
